9e7c2309393b52bc11fb1f14710be9e01df25452
fredbern7 <fredbern7@gmail.com>
2026-01-13 16:23:03 +1100

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,4 @@
-const d = new Date("July 21, 1983 01:15:00");
-let lastModified = `${d.getMonth()}/${d.getDate()}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
-document.getElementById("last-modified").textContent = lastModified;
+const d = new Date(document.lastModified);
+document.getElementById("last-modified").textContent = d.toLocaleString();;
+let currentYear = new Date().getFullYear();
+document.getElementById("current-year").textContent = currentYear;
