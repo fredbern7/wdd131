91243a85c1d927fb337d652101061b7a395036e0
fredbern7 <fredbern7@gmail.com>
2026-06-01 23:53:39 +1100

    update

--- scripts/filtered_temples.js
+++ scripts/filtered_temples.js
@@ -133,7 +133,7 @@ function createTempleCard(temples) {
 
     temples.forEach(temple => {
         let card = document.createElement("section");
-        let name = document.createElement("h3");
+        let name = document.createElement("h2");
         let location = document.createElement("p");
         let dedication = document.createElement("p");
         let area = document.createElement("p");
